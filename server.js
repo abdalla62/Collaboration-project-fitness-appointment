31d536acdd94e7966669a25b6e2310b06150424c
abdalla62 <abdallagraphic62@gmail.com>
2026-01-15 14:11:30 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Test route
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Fitness Appointment Backend is running'
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
